33d4e36f06c17a030b29a8e0e33fc7b113df4a45
Parker Smith <parker@parkersmith.us>
2014-09-06 17:45:43 +0100

    Remove scaffolding

--- main.js
+++ main.js
@@ -9,29 +9,6 @@ define(function (require, exports, module) {
 
     console.log("INITIALIZING {ext_name} EXTENSION");
     
-    var CommandManager = brackets.getModule("command/CommandManager");
-    var Menus          = brackets.getModule("command/Menus");
-    //var EditorManager  = brackets.getModule("editor/EditorManager");
-    //var ProjectManager = brackets.getModule("project/ProjectManager");
-    //var FileUtils = brackets.getModule("file/FileUtils");    
-    //var DocumentManager = brackets.getModule("document/DocumentManager");    
-    //var NativeApp = brackets.getModule("utils/NativeApp");
-    //var Commands = brackets.getModule("command/Commands");
     
-    
-    var COMMAND_ID  = "{ext_name}.{cmd}"; 
-    var MENU_NAME   = "{cmd}";
-    
-    
-    function doMyCommand() {
-        console.log("Executing Command {cmd}");
-    }
-
-
-    CommandManager.register(MENU_NAME, COMMAND_ID, doMyCommand);
-    
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuDivider();
-    menu.addMenuItem(COMMAND_ID);
     
 });
