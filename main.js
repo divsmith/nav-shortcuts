013ea76a85e16fe1e2d089e7a4c8131b01732675
Parker Smith <parker@parkersmith.us>
2014-09-06 18:21:03 +0100

    Define and register commands

--- main.js
+++ main.js
@@ -12,7 +12,15 @@ define(function (require, exports, module) {
         EditorManager  = brackets.getModule("editor/EditorManager"),
         Editor = EditorManager.getFocusedEditor();
     
+    var UP = "divsmith.nav-shortcuts.up";
+    var DOWN = "divsmith.nav-shortcuts.down";
+    var LEFT = "divsmith.nav-shortcuts.left";
+    var RIGHT = "divsmith.nav-shortcuts.right";
     
+    CommandManager.register("Nav-shortcuts UP", UP, handleUp);
+    CommandManager.register("Nav-shortcuts DOWN", UP, handleDown);
+    CommandManager.register("Nav-shortcuts LEFT", UP, handleLeft);
+    CommandManager.register("Nav-shortcuts RIGHT", UP, handleRight);
     
     
 });
